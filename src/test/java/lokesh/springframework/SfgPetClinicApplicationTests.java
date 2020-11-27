98b86e23fc3c50a7b36a81d71d412f3d138ce453
lokeshn1996 <lokesh.github@gmail.com>
2020-11-27 15:53:20 +0530

    my first commit

---
+++ src/test/java/lokesh/springframework/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package lokesh.springframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
